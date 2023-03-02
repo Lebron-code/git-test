5935fe45a5a47a41ffcb3582d49337d35ea1d276
Lebron <1059094918@qq.com>
2023-03-02 16:59:27 +0800

    first commit

---
+++ src/main/java/com/lwh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lwh.git;
+
+/**
+ * @ author 李武杭
+ * @ XUPT
+ * @ 2023/3/2
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+    }
+}
